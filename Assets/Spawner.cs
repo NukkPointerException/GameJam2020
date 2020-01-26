00d2a72129a1859470b7a5358e4ea88197236ac7
bcarroll <bcarroll@guidewire.com>
2020-01-26 19:02:27 +0000

    Adding spawner work

---
+++ Assets/Spawner.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [Header ("Parameters")]
+    [Tooltip ("Time interval to Spawn enemy")]
+    public float timeToSpawn;
+
+    public GameObject enemyPrefab;
+
+    private float timeLeft;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeLeft = timeToSpawn;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+
+        if(timeLeft <= 0.0f)
+        {
+            HandleSpawn();
+        }
+
+    }
+
+    void HandleSpawn()
+    {
+        Instantiate(enemyPrefab, transform.position, Quaternion.identity);
+        timeLeft = timeToSpawn;
+        Debug.Log("Boom");
+    }
+}
